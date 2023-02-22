e5fee135361030511d60055491d51176e00ebfd8
Twilli _ <eclipsek24@gmail.com>
2023-02-21 23:41:03 -0800

    Created a player death system and a way to restart the game.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public enum GameDifficulty { easy, normal, hard }
     public GameDifficulty gameDifficulty;
+    [SerializeField] private Player _player;
 
     private void Awake()
     {
         if (!instance) { instance = this; }
         else { Destroy(gameObject); }
         if (instance == this) { DontDestroyOnLoad(gameObject); }
+    }
+
+    private void Update()
+    {
+        if (_player == null && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+            StartCoroutine("GetNewPlayer");
+        }
+    }
+
+    private IEnumerator GetNewPlayer()
+    {
+        yield return new WaitForSeconds(1);
+        _player = FindObjectOfType<Player>();
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,25 +4,47 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Renderer _playerRenderer;
+    [SerializeField] private Material _defaultPlayerMat;
+    [SerializeField] private GameObject _playerExplosion;
+    public GameObject laserProjectile;
     public float maxHealth, playerSpeed, fireRate, damage;
     [HideInInspector] public float currentHealth;
-    public GameObject laserProjectile;
     [HideInInspector] public bool playerDead;
 
     private void Start()
     {
         transform.localPosition = new Vector3(0, 0, 0);
+        currentHealth = maxHealth;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-
+        if (other.gameObject.GetComponent<Enemy>())
+        {
+            other.gameObject.GetComponent<Enemy>().AdjustHealth(-damage * 2);
+            AdjustHealth(-25);
+        }
     }
 
     public void AdjustHealth(float healthValue)
     {
+        if (healthValue < 0) { StartCoroutine("PlayerHitColorChange"); }
         currentHealth += healthValue;
         if (currentHealth > maxHealth) { currentHealth = maxHealth; }
-        else if (currentHealth < 0) { playerDead = true; }
+        else if (currentHealth < 0) 
+        { 
+            playerDead = true;
+            GameObject newExplosion = Instantiate(_playerExplosion, transform.position, transform.rotation);
+            newExplosion.transform.SetParent(null);
+            Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator PlayerHitColorChange()
+    {
+        _playerRenderer.material.color = new Color(255, 0, 0, 255);
+        yield return new WaitForSeconds(0.1f);
+        _playerRenderer.material = _defaultPlayerMat;
     }
 }
